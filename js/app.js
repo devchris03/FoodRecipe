3009494b04025b61ccc4ecd8ac171758e6908d3f
Christina Sarai Atoche Pascual <christinas.apascual@gmail.com>
2024-11-11 19:48:01 -0500

    feat: Muestra y actualiza receta en el modal

--- js/app.js
+++ js/app.js
@@ -1,20 +1,18 @@
-// ------------------- SELECTORES -------------------
+// --------------------- SELECTORES ---------------------
 const menuBtn = document.querySelector('#menu');
 const closeBtn = document.querySelector('#close');
-const select = document.querySelector('#categorias');
 const result = document.querySelector('#result');
 
-// ------------------- EVENTOS -------------------
+// --------------------- EVENTOS ---------------------
 window.addEventListener('load', () => {
     menuBtn.addEventListener('click', handle);
     closeBtn.addEventListener('click', handle);
-    startApp();
 
+    startApp();
 })
 
-// ------------------- FUNCIONES -------------------
-
-// activa y desactiva menú dezplegable
+// --------------------- FUNCIONES ---------------------
+// Activar y desactivar menu de desplazamiento
 function handle(event) {
     event.preventDefault();
 
@@ -27,21 +25,26 @@ function handle(event) {
 
 function startApp() {
 
-    // EVENTO
-    select.addEventListener('change', selectCategory)
+    const select = document.querySelector('#categorias');
+    select.addEventListener('change', selecCategory);
 
-    getCategory()
+    const modal = document.querySelector('#modal');
+    const closeModal = document.querySelector('#closeModal');
+    closeModal.addEventListener('click', handleModal)
+    
+    getCategories()
 
     // obtener categorias
-    function getCategory() {
+    function getCategories() {
         const url = "https://www.themealdb.com/api/json/v1/1/categories.php";
 
         fetch(url)
             .then(result => result.json())
-            .then(result => showCategories(result.categories))
+            .then(result => showCategories(result.categories));
     }
 
-    // muestra categorias en el html
+
+    // muestra categorias en el select
     function showCategories(categories = []) {
         categories.forEach(category => {
             const option = document.createElement('OPTION');
@@ -52,78 +55,143 @@ function startApp() {
         })
     }
 
-    // obtiene lista de platos por categoria
-    function selectCategory(event) {
+    // selecciona categoria
+    function selecCategory(event) {
+
         if(event.target.value === '') {
-            cleanHtml(result);
+            clearContainer(result)
 
-            const imgEmpty = document.createElement('IMG');
-            imgEmpty.src = "../img/ilustration.svg";
-            imgEmpty.classList.add('ilustrator');
-            imgEmpty.width = "400";
-            imgEmpty.height = "400";
-
-            result.appendChild(imgEmpty);
+            const image = document.createElement('IMG');
+            image.src = "../img/ilustration.svg";
+            image.width = "400";
+            image.height = "400";
+            result.appendChild(image)
 
             return;
         }
 
         const nameCategory = event.target.value;
         const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${nameCategory}`;
-
+        
         fetch(url)
             .then(result => result.json())
-            .then(result => showRecipesList(result.meals))
+            .then(result => showMeal(result.meals))
     }
 
 
-    // muestra recetas en el html
-    function showRecipesList(showRecipesList = []) {
+    // agrega platos al html
+    function showMeal(meals = []) {
+        clearContainer(result)
 
-        cleanHtml(result)
-
-        const title = document.createElement('H1');
-        title.textContent = "Resultados encontrados"
-        title.classList.add('title')
-        
         const recipeContent = document.createElement('SECTION');
         recipeContent.classList.add('recipeContent', 'wrapper');
 
-        result.append(title, recipeContent);
+        const title = document.createElement('H1');
+        title.classList.add('title');
+        title.textContent = "Resultados encontrados";
 
-        showRecipesList.forEach(recipe => {
-            const {idMeal, strMeal, strMealThumb} = recipe;
+        result.append(title, recipeContent)
+
+        // itera resultado
+        meals.forEach(meal => {
+            const {idMeal, strMeal, strMealThumb} = meal;
 
             const recipeCard = document.createElement('ARTICLE');
-            recipeCard.classList.add('recipeCard')
-            
-            const recipeImg = document.createElement('IMG');
-            recipeImg.alt = `Imagen referencial de ${strMeal}`;
-            recipeImg.src = strMealThumb;
+            recipeCard.classList.add('recipeCard');
 
-            const recipeBody = document.createElement('DIV');
+            const recipeImg = document.createElement('IMG');
+            recipeImg.src = strMealThumb;
+            recipeImg.alt = `Imagen referencial del plato ${strMeal}`;
 
             const recipeName = document.createElement('H2');
             recipeName.textContent = strMeal;
 
             const recipeBtn = document.createElement('BUTTON');
-            recipeBtn.classList.add('recipeBtn');
-            recipeBtn.textContent = `Ver Receta`;
+            recipeBtn.classList.add('btn');
+            recipeBtn.textContent = 'Ver receta';
+            recipeBtn.onclick = () => selectRecipe(idMeal);
 
-
-            // inyectar código al html
-            recipeBody.append(recipeName, recipeBtn);
-            recipeCard.append(recipeImg, recipeBody);
-            recipeContent.appendChild(recipeCard);
+            recipeCard.append(recipeImg, recipeName, recipeBtn);
+            recipeContent.appendChild(recipeCard)
         })
     }
 
 
-    // limpia html
-    function cleanHtml(container) {
+    // selecciona receta
+    function selectRecipe(id) {
+        const url = `https://themealdb.com/api/json/v1/1/lookup.php?i=${id}`;
 
-        while(container.firstChild) {
-            container.removeChild(container.firstChild);
+        fetch(url)
+            .then(result => result.json())
+            .then(result => showModal(result.meals[0]));
+    }
+
+    // muestra modal
+    function showModal(recipe) {
+        const { idMeal, strInstructions, strMealThumb, strMeal} = recipe;
+
+        const modalTitle = document.querySelector('#modalTitle');
+        const modalBody = document.querySelector('#modalBody');
+
+        // inserta contenido personalizado al modal
+        modalTitle.textContent = strMeal;
+        modalBody.innerHTML = `
+            <img src=${strMealThumb} alt="Imagen de referencia de ${strMeal}"/>
+            <h3>Instrucciones</h3>
+            <p>${strInstructions}</p>
+            <h3>Ingredientes y cantidad</h3>
+        `;
+
+
+        // inserta ingredientes y cantidad
+        const list = document.createElement('UL');
+
+        for(let i = 1; i <= 20; i++) {
+            if(recipe[`strIngredient${i}`]) {
+                const ingrediente = recipe[`strIngredient${i}`];
+                const cantidad = recipe[`strMeasure${i}`];
+
+                const li = document.createElement('LI');
+                li.textContent = `${ingrediente} - ${cantidad}`;
+
+                list.appendChild(li);
+            }
+        }
+
+        modalBody.appendChild(list);
+
+        const modalButtons = document.querySelector('.modal_buttons');
+        clearContainer(modalButtons);
+
+        // crea los botones para el modal
+        const btnSave = document.createElement('BUTTON');
+        btnSave.textContent = "Guardar favorito";
+        btnSave.ariaLabel = "Guardar receta como favorita";
+        btnSave.classList.add('btn');
+
+        const btnClose = document.createElement('BUTTON');
+        btnClose.textContent = "Cerrar";
+        btnClose.ariaLabel = "Cerrar receta";
+        btnClose.classList.add('btn', 'cerrarbtn');
+        btnClose.onclick = () => handleModal();
+
+        modalButtons.append(btnSave, btnClose);
+
+        handleModal();
+    }
+
+
+    // manipula modal
+    function handleModal() {
+        modal.classList.toggle('active');
+        document.querySelector('body').classList.toggle('overflow', modal.classList.contains('active'))
+    }
+
+
+    // limpia resultado
+    function clearContainer(container) {
+        while(container.firstChild){
+            container.removeChild(container.firstChild)
         }
     }
 }
